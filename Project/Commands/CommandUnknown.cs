d04da7b9cc079eaa012d1b5397359ee22a067c48
GooseToby <tooobiaslotg@gmail.com>
2025-11-07 08:34:03 +0100

    If command/input is empty (Just pressing enter) It returns and prints nothing

--- Project/Commands/CommandUnknown.cs
+++ Project/Commands/CommandUnknown.cs
@@ -1,8 +1,18 @@
 /* Fallback for when a command is not implemented
  */
 
+using System.Net;
+
 class CommandUnknown : BaseCommand, ICommand {
-  public void Execute (Context context, string command, string[] parameters) {
-    Console.WriteLine("I don't understand what '"+command+"' is.😕");
+  public void Execute(Context context, string command, string[] parameters)
+  {
+    if (string.IsNullOrEmpty(command))
+    {
+      return;
+    }
+    else
+    {
+      Console.WriteLine("I don't understand what '" + command + "' is.😕");
+    }
   }
 }
